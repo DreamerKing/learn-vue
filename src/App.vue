2e6d76e4d0b8d9c48bfc62cec74c9f5a923574af
Dreamer King <dreamerking@live.cn>
2023-03-08 16:37:59 +0800

    v-model

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+import CustomInput from './components/CustomInput.vue';
+const test = ref('hello world');
+
+const fatherClick = () => {
+  console.log('father Component clicked')
+}
+// import HelloWorld from './components/HelloWorld.vue'
+// import ReactiveComponet from './components/ReactiveComponet2.vue';
+</script>
+
+<template>
+  <!-- <ReactiveComponet /> -->
+  <CustomInput v-model:name.captial="test" class="test" test="haha" @click="fatherClick" @change="fatherClick"
+    data-k="hi"></CustomInput>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
